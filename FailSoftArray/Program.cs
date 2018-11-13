d5db957f640c5d0d3f3c3d66a3f6a1d56c8e9255
ngtysk0513 <ngtysk0513@gmail.com>
2018-11-13 17:34:03 +0900

    create FailSoftArrayclass

--- FailSoftArray/Program.cs
+++ FailSoftArray/Program.cs
@@ -6,10 +6,41 @@ using System.Threading.Tasks;
 
 namespace FailSoftArray
 {
-    class Program
+    class FailSoftArray
     {
-        static void Main(string[] args)
+        int[] a;
+        int errval;
+
+        public int Length;
+
+        public FailSoftArray(int size, int errv)
         {
+            a = new int[size];
+            errval = errv;
+            Length = size;
         }
+
+        public int Get(int index)
+        {
+            if (Ok(index)) return a[index];
+            return errval;
+        }
+
+        public bool Put(int index, int val)
+        {
+            if (Ok(index))
+            {
+                a[index] = val;
+                return true;
+            }
+            return false;
+        }
+
+        private bool Ok(int index)
+        {
+            if (index >= 0 & index < Length) return true;
+            return false;
+        }
+
     }
 }
